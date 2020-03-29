7072f3677de870eac2d100f03a493138fa50edc3
s16705 <61659821+s16705@users.noreply.github.com>
2020-03-29 20:29:36 +0200

    finall

--- Cwiczenie3/Controllers/StudentsController.cs
+++ Cwiczenie3/Controllers/StudentsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenie3.DAL;
+using Cwiczenie3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +13,39 @@ namespace Cwiczenie3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
-        public string GetStudent()
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
-            return "Kowalski, Malewski, Andrzejewski";
+            _dbService = dbService;
         }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+        
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("200 Aktualizacja dokonczona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie ukonczone");
+        }
+
     }
 }
---
+++ Cwiczenie3/DAL/IDbServiceController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenie3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenie3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
